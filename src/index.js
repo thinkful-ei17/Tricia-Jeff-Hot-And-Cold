6b2b22a7d44cab021711fa877aeb5dbd88926fe3
Jeff Connelly <jeff.connelly6@gmail.com>
2018-01-22 12:19:07 -0700

    Added high level components

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+//Mapping out Components
+
+/* High Level Components
+1. Nav
+2. Header - contains H1
+3. Game
+  - Status
+  - Guess
+  - Guess List
+    -Guess
+*/
+
